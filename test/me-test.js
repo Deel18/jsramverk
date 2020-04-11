fe0bc7db97a60f5195f06ed7dc060749c6f52bdb
deel18 <deel18.github@gmail.com>
2020-04-11 17:16:33 +0100

    Updated selenium test

--- test/me-test.js
+++ test/me-test.js
@@ -11,7 +11,7 @@ test.describe("Me-page", function () {
         browser = new webdriver.Builder().
             withCapabilities(webdriver.Capabilities.firefox()).build();
 
-        browser.get("http://localhost:3000");
+        browser.get("http://deel-ramverk.me");
         done();
     });
 
