182511f0c49f45f71564b499d06b9e86170e378e
deel18 <deel18.github@gmail.com>
2020-05-11 14:51:50 +0100

    Updated chat with db

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -2,31 +2,46 @@ import React from "react";
 import io from "socket.io-client";
 
 const URL = "https://socket-server.deel-ramverk.me";
+//const URL = "localhost:9002"
+
 
 export default class Chat extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
             username: "",
+            userError: "",
             msg: "",
             allMsgs: []
         }
 
         this.socket = io(URL);
+
         this.socket.on("recChatMsg", (msg) => {
             addMessage(msg);
+        })
+
+        this.socket.on("allMsgs", (msg) => {
+            this.setState({ allMsgs: msg })
         })
 
         const addMessage = msg => {
             this.setState({ allMsgs: [...this.state.allMsgs, msg] });
         }
+
         this.sendUser = e => {
             e.preventDefault();
         }
         this.sendMessage = e => {
             e.preventDefault();
 
-            if (this.state.msg.length > 0) {
+            if (this.state.msg.length > 0 && this.state.username.length > 0) {
+                this.setState({ userError: "" })
+                this.socket.emit("userJoined", {
+                    time: new Date().toLocaleTimeString(),
+                    username: this.state.username,
+                    msg: this.state.username + " has joined the chat!"
+                })
                 this.socket.emit("sendMsg", {
                     msg: this.state.msg,
                     username: this.state.username,
@@ -34,6 +49,9 @@ export default class Chat extends React.Component {
                 });
                 this.setState({ msg: "" })
                 document.getElementById("new-message").value = "";
+            } else {
+                this.setState({ userError: "Please provide a username."})
+
             }
         }
     }
@@ -63,7 +81,10 @@ export default class Chat extends React.Component {
             <input placeHolder="Enter your username" className="new-message" style={{width: "10em", height: "1em"}} type="text"
             onChange={e => this.setState({ username: e.target.value })
             } />
-            <p><strong>New message:</strong></p>
+            <div style={{color: "red", fontSize: 18}}>
+            {this.state.userError}
+            </div>
+            <p>New message:</p>
             <input type="text" autoComplete="off" onKeyDown={e => {
                 if (e.keyCode == 13) {
                     this.sendMessage(e);
